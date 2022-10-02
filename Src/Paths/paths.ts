64c5e6fe94035f29f4bd27dff4ebc8815880f5fe
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-10-02 20:35:30 +0200

    Added_Pathes_Variables

---
+++ Src/Paths/paths.ts
@@ -0,0 +1,5 @@
+import path from "path";
+
+export const rootDir = path.join(__dirname, "../..");
+export const imageRoot = path.join(rootDir, "images");
+export const thumbnailDir = path.join(imageRoot, "thumbnail");
